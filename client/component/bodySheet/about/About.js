df99fce5ca748b144e063b6bb2dcd8bba3c949c5
JangGeunSong <sjg1468@gmail.com>
2019-06-19 11:46:01 +0900

    add comment to frontend side

--- client/component/bodySheet/about/About.js
+++ client/component/bodySheet/about/About.js
@@ -12,8 +12,10 @@ export default class About extends Component {
         return (
             <div>
                 <NavBar />
-                <h1> Hello Customer! </h1>
-                <p>This is our About page on the application!.</p>
+                <h1> Hello Visitor! </h1>
+                <h2>What is it?</h2>
+                <p>This is our About page on the application!. This page is shown the product add and delete for the backend side to DBMS</p><br/>
+                <p>I made myself check some technical materials.</p>
                 <p>Made by Song Janggeun</p>
                 <Footer />
             </div>
--- client/component/bodySheet/features/Features.js
+++ client/component/bodySheet/features/Features.js
@@ -12,9 +12,10 @@ export default class Features extends Component {
         return (
             <div>
                 <NavBar />
-                <h1> Hello Customer! </h1>
+                <h1> Hello visitor! </h1>
                 <p>This is our Features page on the application!.</p>
-                <p>This page using redux and MongoDB based web application. If you fill the word on the index page then add the item, and click the delete button the has gone the item on the list.</p>
+                <p>This page using redux and MongoDB based web application. If you fill the word on the index page then add the item, and click the delete button the has gone the item on the list.</p><br/>
+                <p>That process is running on the REST API that linked in MongoDB</p>
                 <Footer />
             </div>
         )
